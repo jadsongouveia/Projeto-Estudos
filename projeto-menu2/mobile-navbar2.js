5c61966b68507c4c68e4645e78d37c8db62ba39c
jadsongouveia <jadsongouveia12@gmail.com>
2024-04-19 15:48:03 -0300

    novos aprendizados

--- projeto-menu2/mobile-navbar2.js
+++ projeto-menu2/mobile-navbar2.js
@@ -1,12 +1,43 @@
-class MobileNavbar {
+class MobileNavbar{
     constructor(mobileMenu, navList, navLinks){
-        this.mobileMenu = document.querySelectorAll(mobileMenu);
-        this.navList = document.querySelectorAll(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active"
 
-        this.handleclick = this.handleclick.bind(this);
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    animateLink() {}
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            console.log(index / 7 + 0.3)
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        });
+    }
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick)
+    }
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent()
+        }
+        return this;
+    }
 }
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+);
+mobileNavbar.init();
